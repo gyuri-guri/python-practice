6cef118ed29a2b2a85c5ac4f5b7e2163040a18a6
gyuri-guri <124rbfl@naver.com>
2021-12-02 22:52:46 +0900

    none

---
+++ python.py
@@ -0,0 +1,19 @@
+# vscode python basic
+
+## 전체실행 : ctrl+F5
+## 한줄실행 : shift+enter
+
+
+print('hello world')
+
+# JUPYTER NOTEBOOK 실행방법
+
+## 1. ANACONDA 설치
+## 2. CMD 실행
+## 3. PYTHON --VERSION 
+## 4. PIP3 INSTALL JUPYTER
+## 5. JUPYTER NOTEBOOK -> 주피터 노트북 실행
+
+
+a = 'hello'
+b = 5
