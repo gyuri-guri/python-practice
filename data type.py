8401826ee50c6f29080152a9e0df8c94b0a941e6
gyuri-guri <124rbfl@naver.com>
2021-12-08 00:01:49 +0900

    DATA TYPE 2

--- data type.py
+++ data type.py
@@ -92,10 +92,87 @@ a.split ()
 a.split('w')   # 구분자 설정
 
 
+## Dictionary
+### '키'와 '값'의 구조. '키'는 내부적으로 hash값으로 저장(동일 키 존재 불가)
+### 인덱스가 없으므로 순서를 따지지않음
+
+a = {'apple':'red','banana':'yellow','watermelon':'green','strawberry':'red','lemon':'yellow'}
+print(a)
+print(a[1])  # error. dictionary는 인덱스를 갖고있지 않음
+print(a['apple'])
+
+### dictionary타입의 항목 추가 및 변경
+
+a['tomato'] = 'red'
+a['orange'] = 'orange'
+a['watermelon'] = 'black&green'
+print(a)
+
+### update() : 두 dictionary를 병합. 겹치는 키가 있다면 parameter로 전달되는 키 값으로 덮어짐
+
+a = {'a':1, 'b':2,'c':3}
+b = {'a':2, 'b':4, 'e':5}
+a.update(b)
+print(a)
+
+### del(), pop() : key삭제
+
+a.pop('b')
+print(a)
+del a['c']
+print(a)
+
+### clear() : Dictionary 모든 값 초기화
+
+print(a)
+a.clear()
+print(a)
+
+### IN() : key값 존재 확인. true or false
+
+a = {'a':1,'b':2,'c':3}
+print(a)
+b = [1,2,3,4,5,6,7,8,9,10]
+print(100 in b) # 리스트 내 원소 개수가 많으면 비효율
+print('b' in a)
+print(2 in a)
+
+### 값 출력 : dictionary명[key명] 또는 get()
+
+a['b']
+print(a.get('b'))
+print(a.get('d'))  # 값이 없으면 None
+print(a['d'])      # error
+
+### 모든 key,values
+
+print(a)
+print(a.keys())
+print(a.values())
+print(list(a.keys()))
+print(list(a.values()))
+list(a.items())          # 튜플형식으로 출력
 
 
+## SET
+### KEY만 활용하는 구조. 수학에서의 집합과 같은 개념. 중복불가하며 순서가 없음
 
+a = {1,1,2,3,4,1,5}
+print(a)
+print(a[1])    # error. 인덱스 없음
 
+### set() : 집합으로 변환
 
+a = [1,1,2,3,3,4,1,5]
+print(a)
+b = set(a)
+print(b)
 
+### 집합의 연산
 
+a = {1,2,3}
+b = {2,3,4}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+print(a.issubset(b))      # 부분집합 판단(T/F)
