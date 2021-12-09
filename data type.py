1ed05460f5b2f23362fcc6690b0947f679f6ccdd
gyuri-guri <124rbfl@naver.com>
2021-12-10 00:18:17 +0900

    list and tuple

--- data type.py
+++ data type.py
@@ -176,3 +176,112 @@ print(a.union(b))
 print(a.intersection(b))
 print(a.difference(b))
 print(a.issubset(b))      # 부분집합 판단(T/F)
+
+
+## LIST 와 TUPLE
+### LIST와 TUEPLE 모두 여러개의 값을 데이터 구조
+### LIST와 TUEPLE의 차이점 : LIST는 생성 후 변경가능(MUTABLE)
+### LIST와 TUEPLE의 차이점 : TUPLE은 생성 후 변경불가능(IMMUTABLE)
+
+a = [] # 빈 리스트 생성
+a = [1,2,3,4,5,10]
+a = ['apple','banana',1,23,[33,55]]  # 원소로 리스트도 가능
+
+### 문자열을 리스트로 바꾸면?
+a = 'hello world'
+b = list(a)
+print(a)
+print(b)
+
+### 튜플을 리스트로 바꾸면?
+c = ('a','b','c')
+d = list(c)
+print(c)
+print(d)
+
+### split() : 구분자로 구분하여 리스트로 반환
+a = 'hello world nice weather'
+b = a.split()  # split('구분자')
+c = a.split('o')
+print(a)
+print(b)
+print(c)
+
+## LIST INDEXING
+a = [1,2,3,4,5,6]
+print(a[0])
+print(a[2])
+print(a[-1])
+
+## LIST UPDATE
+### 일반 문자열은 불변이기에 추가하거나 대체만 가능. 
+a = 'hello world'
+b = 'b' + a[1:]   # 추가
+c = a.replace('h', 'd')  # 대체
+print(a)
+print(b)
+print(c)
+
+### 리스트는 불변 가능이기에 인덱스를 이용한 수정 가능
+a = [1,2,3,4,5,6,7]
+a[0] = 200
+a[5] = 555
+print(a)
+
+## LIST SLICING
+a = [1,2,3,4,5,6,7,8,9,10]
+print(a[4:7])
+print(a[1:5:1])   # index:index:interval
+print(a[1:6:2])
+
+## LIST관련 함수
+### append() : 리스트 끝에 항목 추가
+a = [1,2,3,4,5]
+a.append(8)
+print(a)
+
+### extend() : 리스트 연장. +,-로 가능함
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+a.extend(b)
+print(a)
+
+c = [11,12,13,14,15]
+c += b 
+print(c)
+
+### insert() : 항목추가
+a = [1,2,3,4,5]
+a.insert(1, 40)  # index, value
+print(a)
+
+### remove() : 항목삭제
+a = [1,2,3,4,5]
+a.remove(2)     #value
+print(a)
+
+a = [1,2,5,3,5]
+a.remove(5)     # 지우려는 값이 여러개있으면 가장 앞 값 삭제
+print(a)
+
+### pop() : 지우고 싶은 아이템  반환 후 삭제
+a = [1,2,3,4,5]
+a.pop()      # index. default 값은 맨 마지막 값
+print(a)
+
+a.pop(1)
+print(a)
+
+b = a.pop(2)
+print(b)        # 지운값을 다시 변수에 지정 가능
+
+### index() : 찾고자하는 값의 인덱스 반환
+a = [1,2,3,4,5]
+a.index(2)
+
+### in : 리스트 내 해당값 존재여부. value in [list] 
+a = [1,2,3,4,5]
+b = 7
+
+c = b in a
+print(c)
