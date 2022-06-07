43f6a0490b94d748e7d6f3a5c21d0ab94c6b0a75
gyuri-guri <124rbfl@naver.com>
2022-06-07 23:20:02 +0900

    update

--- python numpy/numby and matplotlib.py
+++ python numpy/numby and matplotlib.py
@@ -9,6 +9,22 @@ y = np.array([2,4,6])
 
 plt.plot(x,y)
 
+## ndarray 생성하기 (with numpy)
+### np.array
+x = np.array([1,2,3,4]) # 1차원
+print(x)
 
+y = np.array([[2,3,4],[1,2,5]]) 
+print(y)
 
+print( type(y) )
 
+### np.arange 
+np.arange(10)
+np.arange(1,10)
+np.arange(1,10,2)
+np.arange(5,101,5)
+
+### np.ones, np.zeros
+np.ones((4, 5))   # 튜플을 이용한 명시
+np.ones((2, 3, 4))
